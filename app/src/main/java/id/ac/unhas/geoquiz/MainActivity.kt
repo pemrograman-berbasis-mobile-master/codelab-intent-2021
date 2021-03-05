fc7105b14f9756d141f5da2d7d0941cb7ac70abc
Ais Prayogi <ais.prayogi@gmail.com>
2021-03-05 10:19:52 +0800

    codelab1 done

---
+++ app/src/main/java/id/ac/unhas/geoquiz/MainActivity.kt
@@ -0,0 +1,32 @@
+package id.ac.unhas.geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+            //Todo response to the click here
+            Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_SHORT).show()
+        }
+
+        falseButton.setOnClickListener { view: View ->
+            //Todo response to the click here
+
+        }
+
+    }
+}
