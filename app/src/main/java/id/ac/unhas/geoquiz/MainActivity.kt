1951f9707886264bf1cf1f5a43862005c2c19ec5
Ais Prayogi <ais.prayogi@gmail.com>
2021-03-05 10:50:02 +0800

    codelab2 done

--- app/src/main/java/id/ac/unhas/geoquiz/MainActivity.kt
+++ app/src/main/java/id/ac/unhas/geoquiz/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -11,22 +12,64 @@ class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
 
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+    private var currentIndex = 0
+
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
+
+
+        val questionTextResId = questionBank[currentIndex].textResId
+
+        questionTextView.setText(questionTextResId)
 
         trueButton.setOnClickListener { view: View ->
             //Todo response to the click here
-            Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_SHORT).show()
+            checkAnswer(true)
+        }
+
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            val questionTextResId = questionBank[currentIndex].textResId
+            questionTextView.setText(questionTextResId)
         }
 
         falseButton.setOnClickListener { view: View ->
             //Todo response to the click here
-
+            checkAnswer(false)
         }
 
     }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+            .show()
+    }
+
 }
---
+++ app/src/main/java/id/ac/unhas/geoquiz/Question.kt
@@ -0,0 +1,5 @@
+package id.ac.unhas.geoquiz
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
