88d473b1062d8669ee221cbdd708ed1fae35afac
Ais Prayogi <ais.prayogi@gmail.com>
2021-03-12 01:12:19 +0800

    codelab3

--- app/src/main/java/id/ac/unhas/geoquiz/MainActivity.kt
+++ app/src/main/java/id/ac/unhas/geoquiz/MainActivity.kt
@@ -2,10 +2,13 @@ package id.ac.unhas.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,10 +28,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var nextButton: Button
     private lateinit var questionTextView: TextView
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG,"onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
@@ -57,6 +59,31 @@ class MainActivity : AppCompatActivity() {
             checkAnswer(false)
         }
 
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"onDestroy called")
     }
 
     private fun checkAnswer(userAnswer: Boolean) {
